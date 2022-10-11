95dba55e16d95d3927df453c02edce6ba86280d9
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-10-11 14:31:30 +0100

    mapped mock data

--- src/components/collectionTable/index.tsx
+++ src/components/collectionTable/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Image, List, ListItem, Text } from "react-xnft";
+import MockData from "../../utils/mockData.json";
 
 export const CollectionTable = () => {
   return (
@@ -96,140 +97,145 @@ export const CollectionTable = () => {
         </ListItem>
       </List>
 
-      {/* mapped data */}
-      <List
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          margin: "auto",
-          width: "95vw",
-          marginTop: "1vh",
-        }}
-      >
-        <ListItem
-          style={{
-            width: "16vw",
-            height: "fit-content",
-            textAlign:"center"
-          }}
-        >
-          <Image
+      {MockData.map((listItem) => {
+        const tvl = parseInt(listItem.tvl) / 1000;
+        return (
+          <List
             style={{
-              width: "8vw",
-              padding: "3.5vh 2vw 0.5vh 2vw",
+              display: "flex",
+              justifyContent: "space-between",
+              margin: "auto",
+              width: "95vw",
+              marginTop: "1vh",
             }}
-            src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-          />
-          <Text
-            style={{
-              textAlign: "center",
-              overflowWrap: "break-word",
-              paddingBottom: "3vh",
-            }}
+            key={listItem.id}
           >
-            ABC
-          </Text>
-        </ListItem>
+            <ListItem
+              style={{
+                width: "16vw",
+                height: "fit-content",
+                textAlign: "center",
+              }}
+            >
+              <Image
+                style={{
+                  width: "8vw",
+                  padding: "3.5vh 2vw 0.5vh 2vw",
+                }}
+                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
+              />
+              <Text
+                style={{
+                  textAlign: "center",
+                  overflowWrap: "break-word",
+                  paddingBottom: "3vh",
+                }}
+              >
+                {listItem.name}
+              </Text>
+            </ListItem>
 
-        <ListItem
-          style={{
-            width: "20vw",
-            height: "fit-content",
-          }}
-        >
-          <Text
-            style={{
-              textAlign: "center",
-              overflowWrap: "break-word",
-              padding: "3vh 2vw 0 2vw",
-            }}
-          >
-            10K
-          </Text>
-          <Text
-            style={{
-              textAlign: "center",
-              overflowWrap: "break-word",
-              padding: "0.5vh 0 3vh 0",
-            }}
-          >
-            377
-          </Text>
-        </ListItem>
+            <ListItem
+              style={{
+                width: "20vw",
+                height: "fit-content",
+              }}
+            >
+              <Text
+                style={{
+                  textAlign: "center",
+                  overflowWrap: "break-word",
+                  padding: "3vh 2vw 0 2vw",
+                }}
+              >
+                10K
+              </Text>
+              <Text
+                style={{
+                  textAlign: "center",
+                  overflowWrap: "break-word",
+                  padding: "0.5vh 0 3vh 0",
+                }}
+              >
+                {listItem.listings}
+              </Text>
+            </ListItem>
 
-        <ListItem
-          style={{
-            width: "16vw",
-            height: "fit-content",
-            textAlign: "center",
-          }}
-        >
-          <Image
-            style={{
-              width: "8vw",
-              padding: "3.5vh 2vw 0.5vh 2vw",
-            }}
-            src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-          />
-          <Text
-            style={{
-              textAlign: "center",
-              overflowWrap: "break-word",
-              paddingBottom: "3vh",
-            }}
-          >
-            {(29.615).toFixed(2)}
-          </Text>
-        </ListItem>
-        <ListItem
-          style={{
-            width: "16vw",
-            height: "fit-content",
-            textAlign: "center",
-          }}
-        >
-          <Image
-            style={{
-              width: "8vw",
-              padding: "3.5vh 2vw 0.5vh 2vw",
-            }}
-            src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-          />
-          <Text
-            style={{
-              textAlign: "center",
-              overflowWrap: "break-word",
-              paddingBottom: "3vh",
-            }}
-          >
-            {(27.965).toFixed(2)}
-          </Text>
-        </ListItem>
-        <ListItem
-          style={{
-            width: "16vw",
-            height: "fit-content",
-            textAlign: "center",
-          }}
-        >
-          <Image
-            style={{
-              width: "8vw",
-              padding: "3.5vh 2vw 0.5vh 2vw",
-            }}
-            src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-          />
-          <Text
-            style={{
-              textAlign: "center",
-              overflowWrap: "break-word",
-              paddingBottom: "3vh",
-            }}
-          >
-            {(2649.694 / 1000).toFixed(2)}K
-          </Text>
-        </ListItem>
-      </List>
+            <ListItem
+              style={{
+                width: "16vw",
+                height: "fit-content",
+                textAlign: "center",
+              }}
+            >
+              <Image
+                style={{
+                  width: "8vw",
+                  padding: "3.5vh 2vw 0.5vh 2vw",
+                }}
+                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
+              />
+              <Text
+                style={{
+                  textAlign: "center",
+                  overflowWrap: "break-word",
+                  paddingBottom: "3vh",
+                }}
+              >
+                {parseInt(listItem.floor).toFixed(2)}
+              </Text>
+            </ListItem>
+            <ListItem
+              style={{
+                width: "16vw",
+                height: "fit-content",
+                textAlign: "center",
+              }}
+            >
+              <Image
+                style={{
+                  width: "8vw",
+                  padding: "3.5vh 2vw 0.5vh 2vw",
+                }}
+                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
+              />
+              <Text
+                style={{
+                  textAlign: "center",
+                  overflowWrap: "break-word",
+                  paddingBottom: "3vh",
+                }}
+              >
+                {parseInt(listItem.offer).toFixed(2)}
+              </Text>
+            </ListItem>
+            <ListItem
+              style={{
+                width: "16vw",
+                height: "fit-content",
+                textAlign: "center",
+              }}
+            >
+              <Image
+                style={{
+                  width: "8vw",
+                  padding: "3.5vh 2vw 0.5vh 2vw",
+                }}
+                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
+              />
+              <Text
+                style={{
+                  textAlign: "center",
+                  overflowWrap: "break-word",
+                  paddingBottom: "3vh",
+                }}
+              >
+                {tvl.toFixed(2)}K
+              </Text>
+            </ListItem>
+          </List>
+        );
+      })}
     </>
   );
 };
