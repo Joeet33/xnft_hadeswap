882500db8663a322d5176173cd586b8bff3d2a8d
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-10-12 10:02:40 +0100

    api added

--- src/components/collectionTable/index.tsx
+++ src/components/collectionTable/index.tsx
@@ -1,8 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, List, ListItem, Text } from "react-xnft";
 import MockData from "../../utils/mockData.json";
 
+interface Props {
+  marketPubkey: string;
+  collectionName: string;
+  collectionImage: string;
+  listingsAmount: number;
+  floorPrice: string;
+  bestoffer: string;
+  offerTVL: string;
+  nftValidationAdapter: string;
+}
+
 export const CollectionTable = (props) => {
+  const [markets, setMarkets] = useState<Props[]>();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://hadeswap-api.herokuapp.com/markets"
+      );
+      const data = await response.json();
+      setMarkets(data);
+    };
+    fetchData();
+  }, []);
+
+  console.log("testdata", markets);
+
   return (
     <>
       <List
@@ -97,153 +123,68 @@ export const CollectionTable = (props) => {
         </ListItem>
       </List>
 
-      {MockData.filter((listItem) => {
-        if (props.query === "") {
-          return listItem;
-        } else if (
-          listItem.name.toLowerCase().includes(props.query.toLowerCase())
-        ) {
-          return listItem;
-        }
-      }).map((listItem) => {
-        const tvl = parseInt(listItem.tvl) / 1000;
-        return (
-          <List
-            style={{
-              display: "flex",
-              justifyContent: "space-between",
-              margin: "auto",
-              width: "95vw",
-              marginTop: "1vh",
-            }}
-            key={listItem.id}
-          >
-            <ListItem
+      {markets
+        ?.filter((listItem) => {
+          if (props.query === "") {
+            return listItem;
+          } else if (
+            listItem.collectionName
+              .toLowerCase()
+              .includes(props.query.toLowerCase())
+          ) {
+            return listItem;
+          }
+        })
+        .map((listItem, i) => {
+          if (i > 5) return;
+          return (
+            <List
               style={{
-                width: "16vw",
-                height: "fit-content",
-                textAlign: "center",
+                display: "flex",
+                justifyContent: "space-between",
+                margin: "auto",
+                width: "95vw",
+                marginTop: "1vh",
               }}
+              key={i}
             >
-              <Image
-                style={{
-                  width: "8vw",
-                  padding: "3.5vh 2vw 0.5vh 2vw",
-                }}
-                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-              />
-              <Text
-                style={{
-                  textAlign: "center",
-                  overflowWrap: "break-word",
-                  paddingBottom: "3vh",
-                }}
-              >
-                {listItem.name}
-              </Text>
-            </ListItem>
-
-            <ListItem
-              style={{
-                width: "20vw",
-                height: "fit-content",
-              }}
-            >
-              <Text
-                style={{
-                  textAlign: "center",
-                  overflowWrap: "break-word",
-                  padding: "3vh 2vw 0 2vw",
-                }}
-              >
-                10K
-              </Text>
-              <Text
-                style={{
-                  textAlign: "center",
-                  overflowWrap: "break-word",
-                  padding: "0.5vh 0 3vh 0",
-                }}
-              >
-                {listItem.listings}
-              </Text>
-            </ListItem>
-
-            <ListItem
-              style={{
-                width: "16vw",
-                height: "fit-content",
-                textAlign: "center",
-              }}
-            >
-              <Image
-                style={{
-                  width: "8vw",
-                  padding: "3.5vh 2vw 0.5vh 2vw",
-                }}
-                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-              />
-              <Text
-                style={{
-                  textAlign: "center",
-                  overflowWrap: "break-word",
-                  paddingBottom: "3vh",
-                }}
-              >
-                {parseInt(listItem.floor).toFixed(2)}
-              </Text>
-            </ListItem>
-            <ListItem
-              style={{
-                width: "16vw",
-                height: "fit-content",
-                textAlign: "center",
-              }}
-            >
-              <Image
-                style={{
-                  width: "8vw",
-                  padding: "3.5vh 2vw 0.5vh 2vw",
-                }}
-                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-              />
-              <Text
-                style={{
-                  textAlign: "center",
-                  overflowWrap: "break-word",
-                  paddingBottom: "3vh",
-                }}
-              >
-                {parseInt(listItem.offer).toFixed(2)}
-              </Text>
-            </ListItem>
-            <ListItem
-              style={{
-                width: "16vw",
-                height: "fit-content",
-                textAlign: "center",
-              }}
-            >
-              <Image
-                style={{
-                  width: "8vw",
-                  padding: "3.5vh 2vw 0.5vh 2vw",
-                }}
-                src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
-              />
-              <Text
-                style={{
-                  textAlign: "center",
-                  overflowWrap: "break-word",
-                  paddingBottom: "3vh",
-                }}
-              >
-                {tvl.toFixed(2)}K
-              </Text>
-            </ListItem>
-          </List>
-        );
-      })}
+              {Object.keys(listItem).map((itemData) => {
+                const listText =
+                  itemData === "collectionName"
+                    ? listItem[itemData]
+                    : parseFloat(listItem[itemData]).toFixed(2);
+                return (
+                  <ListItem
+                    style={{
+                      width: "16vw",
+                      height: "fit-content",
+                      textAlign: "center",
+                    }}
+                  >
+                    <Image
+                      style={{
+                        width: "8vw",
+                        padding: "3.5vh 2vw 0.5vh 2vw",
+                      }}
+                      src={"https://cryptologos.cc/logos/solana-sol-logo.png"}
+                    />
+                    <Text
+                      style={{
+                        textAlign: "center",
+                        overflowWrap: "break-word",
+                        paddingBottom: "3vh",
+                      }}
+                    >
+                      {itemData === "offertvl"
+                        ? `${(parseInt(listItem[itemData]) / 1000).toFixed(2)}K`
+                        : listText}
+                    </Text>
+                  </ListItem>
+                );
+              })}
+            </List>
+          );
+        })}
     </>
   );
 };
