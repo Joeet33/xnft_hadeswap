56496b5af7f5edbfbd3016bb917cd669f40287ff
Joe Ellul-Turner <joeet@pop-os.localdomain>
2022-10-14 22:58:37 +0100

    styled commit page

--- src/components/trade/index.tsx
+++ src/components/trade/index.tsx
@@ -9,14 +9,46 @@ export const Trade = () => {
   };
 
   return (
-    <View style={{display:"flex", flexDirection:"column" ,position:"relative", top:"50%", textAlign:"center"}}>
-      <View style={{dislay:"flex", flexDirection:"row"}}>
-        <Text>24 hour volume</Text>
-        <Text>all time volume</Text>
+    <View style={{ position: "relative", textAlign: "center", top: "25%" }}>
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          paddingBottom: "2vh",
+        }}
+      >
+        <Text
+          style={{
+            padding: "5vw",
+            marginRight: "2vw",
+            borderRadius:"2vw",
+            backgroundColor: "darkblue",
+          }}
+        >
+          24h volume
+        </Text>
+        <Text
+          style={{
+            padding: "5vw",
+            marginLeft: "2vw",
+            borderRadius:"2vw",
+            backgroundColor: "darkblue",
+          }}
+        >
+          All time volume
+        </Text>
       </View>
-      <Text>total value locked</Text>
-      <Text>The best NFT marketplace on Solana for traders</Text>
-      <Button style={{margin:"auto"}} onClick={onClickHandler}>Trade</Button>
+      <Text style={{padding: "5vw",
+       width:"50%",
+       margin:"auto",
+            borderRadius:"2vw",
+            backgroundColor: "darkblue", }}>
+        Total value locked
+      </Text>
+      <Text style={{ padding: "3vh 4vw 4vh 4vw" }}>
+        The best NFT marketplace on Solana for traders
+      </Text>
+      <Button onClick={onClickHandler}>Trade</Button>
     </View>
   );
 };
